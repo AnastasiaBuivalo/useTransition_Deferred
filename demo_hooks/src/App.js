3ae365f276a753bdb4ab6e62bcd3a18b00342934
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-11-11 15:13:26 +0300

    useTransition demo

---
+++ demo_hooks/src/App.js
@@ -0,0 +1,40 @@
+import data from './data';
+import {useState, useMemo, useDeferredValue, useTransition} from 'react';
+
+function App() {
+    const [text, setText] = useState('');
+    const [posts, setPosts] = useState(data);
+    //const deferedValue = useDeferredValue(text);
+    const [isPending, startTransition] = useTransition()
+
+    const filteredPosts = useMemo(() => {
+        return posts.filter(item => item.name.toLowerCase().includes(text));
+    }, [text]);
+
+    const onValueChange = (e) => {
+        startTransition(()=>{
+            setText(e.target.value);
+        })
+       
+    }
+
+    return (
+        <>
+            <input value={text} type='text' onChange={onValueChange}/>
+
+            <hr/>
+
+            <div>
+                {isPending? <h2>Loading...</h2>:
+                filteredPosts.map(post => (
+                    <div key={post._id}>
+                        <h4>{post.name}</h4>
+                    </div>
+                ))}
+                
+            </div>
+        </>
+    );
+}
+
+export default App;
